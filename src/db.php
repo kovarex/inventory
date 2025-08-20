7ad874e9b0e576e6eeb0ec67645d773ce44b8060
kovarex <kovarex@gmail.com>
2025-08-20 17:10:49 +0200

    Maybe not public password

--- src/db.php
+++ src/db.php
@@ -1,5 +1,6 @@
 <?php
-$db = new mysqli("c168um.forpsi.com", "kovarexcom", "tPXJufGQ", "kovarexcom");
+include("../config.php");
+$db = new mysqli("a066um.forpsi.com", "f190987", $dbPassword, "f190987");
 if ($db->connect_error)
   die("Connection failed: " . $db->connect_error);
 ?>
