62b60afee3c77f5d85a1b774847906134d3dad5a
kovarex <kovarex@gmail.com>
2025-08-24 10:40:19 +0200

    show transaction count only from my home

--- users.php
+++ users.php
@@ -4,14 +4,22 @@ echo "<h1>Users</h1>";
 
 $users = query("SELECT
                  im_user.username as username,
-                 count(im_transaction.id) as transaction_count
+                 count(t.transaction_id) as transaction_count
                FROM
+                 im_home_user,
                  im_user left join
-                 im_transaction on im_transaction.user_id = im_user.id,
-                 im_home_user
+                 (SELECT
+                   im_transaction.id as transaction_id,
+                   im_transaction.user_id as user_id
+                 FROM
+                   im_transaction, im_item
+                 WHERE
+                   im_transaction.item_id = im_item.id and
+                   im_item.home_id = ".homeID()."
+                 ) t on im_user.id = t.user_id
                WHERE
                  im_home_user.user_id = im_user.id and
-                 im_home_user.home_id=".homeID()."
+                 im_home_user.home_id = ".homeID()."
                GROUP BY
                  im_user.id")->fetch_all(MYSQLI_ASSOC);
 echo "<table class=\"data-table\"><tr><th>Username</th><th>Transactions</th></tr>";
