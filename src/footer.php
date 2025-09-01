95e4d9caeab8a6049b55b78b63ff99c6fb0776cd
kovarex <kovarex@gmail.com>
2025-09-01 10:01:16 +0200

    Footer fix.

--- src/footer.php
+++ src/footer.php
@@ -1,2 +1,7 @@
+<div style="position:screen;bottom:0px;">
+  <hr/>
+  Made during summer go camp 2025 by Honza Adámek and Michal Kovařík and not Vít Brunner, who thought this should be just an excel table!
+</div>
+
 </body>
 </html>
