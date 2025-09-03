e9ea9576a80f5fbf0cfe4c50ce191afe0639fc0d
kovarex <kovarex@gmail.com>
2025-09-03 11:03:05 +0200

    Fixed item form when location is predefined.

--- src/item_helper.php
+++ src/item_helper.php
@@ -69,9 +69,11 @@ function itemForm($formAction, $itemToEdit, $redirect, $predefinedLocation = NUL
     </tr>
 <?php
     if (empty($predefinedLocation))
+    {
       echo "<tr><td><label for=\"location_id\">Location:</label></td><td>";
       locationSelector("location_id", @$itemToEdit["location_id"]);
       echo "</td></tr>";
+    }
     ?>
     <tr>
       <td><label for="image">Image:</label></td>
